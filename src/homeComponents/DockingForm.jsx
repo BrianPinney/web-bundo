0e4d47617e08a4e83200ae99d6844469c602a742
Brian J. Pinney <brianj.pinney@gmail.com>
2023-05-07 12:29:45 -0400

    First commit started React app

---
+++ src/homeComponents/DockingForm.jsx
@@ -0,0 +1,14 @@
+
+
+export default function DockingForm (){
+
+    return (
+        <>
+        <form>
+        <input type="text" placeholder="Lambda" />
+        <br />
+        <input type="text" placeholder="Phi" />
+        </form>
+        </>
+    )
+}
---
+++ src/homeComponents/Docks.jsx
@@ -0,0 +1,8 @@
+export default function Dock (){
+
+    return (
+        <>
+        Docks
+        </>
+    )
+}
